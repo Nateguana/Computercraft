cfe3d623019a15d149cf8783f3adc5bb51183f0c
nateguana <nateguana@gmail.com>
2025-12-05 17:33:47 -0500

    pokemon now have correct pixel dimensions

--- src/programs/pokemon.ts
+++ src/programs/pokemon.ts
@@ -5,40 +5,31 @@ import { POKEMON_DATA } from "./pokemon_data";
 import { decode } from "../lua/base64"
 import { DecompressZlib } from "../lua/libdeflate"
 
-const POKEMON_BYTES = DecompressZlib(decode(POKEMON_DATA))!
+// function drawPixel(terminal: ITerminal, xPos: number, yPos: number) {
+//     terminal.setCursorPos(xPos, yPos)
+//     terminal.write(" ")
+// }
 
-function drawPixel(terminal: ITerminal, xPos: number, yPos: number) {
-    terminal.setCursorPos(xPos, yPos)
-    terminal.write(" ")
-}
-
-function drawPokemonPixel(terminal: ITerminal, color: number, index: number) {
-    terminal.setBackgroundColour(2 ** color);
-    let x = index % 37;
-    let y = Math.floor(index / 37);
-    drawPixel(terminal, x + 1, y + 1)
-}
-
-function drawPokemon(terminal: ITerminal, number: number) {
-    let palette_start = (number - 1) * 512;
+function drawPokemon(terminal: ITerminal, poke_data: string, number: number) {
+    let palette_start = (number - 1) * 480;
     for (let index = 0; index < 16; index++) {
         let palette_index = palette_start + index * 3;
-        let [b1, b2, b3] = string.byte(POKEMON_BYTES, palette_index + 1, palette_index + 3);
+        let [b1, b2, b3] = string.byte(poke_data, palette_index + 1, palette_index + 3);
         let color = (b1 << 16) | (b2 << 8) | b3;
         terminal.setPaletteColor(2 ** index, color);
     }
     let pixel_start = palette_start + 16 * 3;
-    // terminal.setCursorPos(1, 1)
-    for (let index = 0; index < 37 * 25; index += 2) {
-        let byte = string.byte(POKEMON_BYTES, pixel_start + index / 2 + 1)
-        let high = byte >>> 4;
-        let low = byte & 0xf;
-        // terminal.setBackgroundColour(2 ** high);
-        // terminal.write("ee")
-        // terminal.setBackgroundColour(2 ** low);
-        // terminal.write("e")
-        drawPokemonPixel(terminal, index % 2, index);
-        drawPokemonPixel(terminal, (index + 1) % 2, index + 1);
+    for (let y = 0; y < 24; y++) {
+        terminal.setCursorPos(1, y + 1)
+        for (let x = 0; x < 18; x++) {
+            let byte = string.byte(poke_data, pixel_start + y * 18 + x + 1)
+            let high = byte >>> 4;
+            let low = byte & 0xf;
+            terminal.setBackgroundColour(2 ** high);
+            terminal.write(" ")
+            terminal.setBackgroundColour(2 ** low);
+            terminal.write(" ")
+        }
     }
 }
 
@@ -53,7 +44,11 @@ function pickPokemon(): number {
 }
 
 export default function Main() {
-    let monitor = peripheral.wrap("left") as MonitorPeripheral
+    print("Loading Pokemon Data")
+    const poke_data = DecompressZlib(decode(POKEMON_DATA))!
+    print("Finished")
+
+    let monitor = peripheral.wrap("left") as MonitorPeripheral;
     monitor.setTextScale(.5)
     // term.setTextScale(.5)
     // term.redirect(monitor)
@@ -61,17 +56,17 @@ export default function Main() {
     monitor.clear()
 
 
-    // while (true) {
-    //     let last_index = 0;
-    //     let index = 0;
-    //     do {
-    //         index = pickPokemon()
-    //     } while (last_index == index)
-    //     drawPokemon(monitor, index);
-    //     sleep(.5)
-    // }
+    while (true) {
+        let last_index = 0;
+        let index = 0;
+        do {
+            index = pickPokemon()
+        } while (last_index == index)
+        drawPokemon(monitor, poke_data, index);
+        sleep(.5)
+    }
 
-    drawPokemon(monitor, 1)
+    // drawPokemon(monitor, poke_data, 428)
     // for (let index = 0; index < pallete.length; index++) {
     //     monitor.setPaletteColor(2 ** index, pallete[index])
     // }
